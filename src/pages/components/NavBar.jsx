f17b47ab9ba85246baa092d4831908ad36623d98
GrabowskaJoanna <joanna.trojanek18@gmail.com>
2024-10-16 17:23:03 +0200

    styled nav bar and added variables

--- src/pages/components/NavBar.jsx
+++ src/pages/components/NavBar.jsx
@@ -6,12 +6,23 @@ const NavBar = () => {
     <div className="nav">
       <NavLink to="/" className="nav_logo">
         <img src={Logo} alt="logo" />
+        <h1>Adopt-Tail</h1>
       </NavLink>
       <div className="nav_links">
-        <NavLink to="/adoptionForm" className="nav_link">
+        <NavLink
+          to="/adoptionForm"
+          className={({ isActive }) =>
+            isActive ? "nav_link active" : "nav_link"
+          }
+        >
           ADOPTION FORM
         </NavLink>
-        <NavLink to="/pets" className="nav_link">
+        <NavLink
+          to="/pets"
+          className={({ isActive }) =>
+            isActive ? "nav_link active" : "nav_link"
+          }
+        >
           OUR PETS
         </NavLink>
       </div>
